1908c0366b91e23d8a8391f762ac5f98e9a4db22
Biao Wang <biaowwang@global.tencent.com>
2024-06-09 11:33:58 -0700

    update example.sh

--- example.sh
+++ example.sh
@@ -1,1 +1,5 @@
-coan source -DDEBUG_BUFFER=0 --filter cpp,h --recurse directory
+cd /mnt/c/Users/Admin/home/git/reftem/t-ecm
+#the first source is the type we would like to process, the second source is the directory we specified
+#to specify desired macro to remove, using -D
+#the binary executable coan has been added in /usr/bin/
+coan source -DDEBUG_BUFFER=0 --filter cpp,h --recurse source
